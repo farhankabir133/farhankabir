fea09f2135153ad382da5b2333f0d40bc1a3d2e9
A. S. M. Farhan Kabir Redoy <102225132+farhankabir133@users.noreply.github.com>
2025-09-08 15:44:14 +0000

    Deploy to GitHub Pages

---
+++ assets/all-wallets-Ccv2bN6w.js
@@ -0,0 +1,6 @@
+import{n as o}from"./core-CAOb5GyD.js";import"./index-D4CcWFcN.js";import"./index.es-DSvL3LOK.js";import"./events-DQ172AOg.js";import"./node-fsQZ-NIu.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{p as allWalletsSvg};
